224d316101a46a986260616be46c298b70570652
Yinston <n3152u@gmail.com>
2026-03-09 18:43:33 +0300

    search v1.2

--- lab7.py
+++ lab7.py
@@ -3,8 +3,7 @@ import json
 
 
 
-def pages():
-    next_page = 'https://rickandmortyapi.com/api/character'
+def pages(next_page):
     pages_list = []
     for i in range(42):
         response = requests.get(next_page)
@@ -14,30 +13,34 @@ def pages():
     return pages_list
 
 
-def search(character=str):
-    PARAM_LIST = ['id', 'name', 'status', 'species', 'type', 'gender', 'image']
-    KEY_RY = {'id':'id', 'name':'Имя', 'status':'Статус', 'species':'Вид', 
-              'type':'Тип (при наличии)', 'gender':'Гендер', 'image':'Ссылка на изображение'}
-
-    flag = 0
-    for j in range(len(PAGES_LIST)):
-        if PAGES_LIST[j]['name'] == character:
-            flag = 1
+def search(character=str, mode=1):
+    if mode == 0:    
+        response = requests.get(f'https://rickandmortyapi.com/api/character/?name={character}')
+        response = response.json()
+        for j in range(len(response['results'])):
             for key in PARAM_LIST:
-                print(f'{KEY_RY[key]}: {PAGES_LIST[j][key]}')
+                print(f'{KEY_RY[key]}: {response['results'][j][key]}')
             print()
-        elif flag == 1:
-            flag == -1
-            break
+    else:
+        for j in range(len(pages_list)):
+            if pages_list[j]['name'] == character:
+                for key in PARAM_LIST:
+                    print(f'{KEY_RY[key]}: {pages_list[j][key]}')
+                print()
     print("===================That's all====================\n")
-
+    
 
 CHARACTERS = ['Baby Legs', 'Beth Smith', 'Abradolf Lincler']
-PAGES_LIST = pages()
+PARAM_LIST = ['id', 'name', 'status', 'species', 'type', 'gender', 'image']
+KEY_RY = {'id':'id', 'name':'Имя', 'status':'Статус', 'species':'Вид', 
+          'type':'Тип (при наличии)', 'gender':'Гендер', 'image':'Ссылка на изображение'}
 
 
-for character in CHARACTERS:
-    search(character)
 
+if __name__ == '__main__':
+    mode = 1 # режим работы - быстрый-0 или расширенный-1
+    if mode == 1:
+        pages_list = pages('https://rickandmortyapi.com/api/character')
 
-# ['id', 'name', 'status', 'species', 'type', 'gender', 'origin', 'location', 'image', 'episode', 'url', 'created']
+    for character in CHARACTERS:
+        search(character, mode)
